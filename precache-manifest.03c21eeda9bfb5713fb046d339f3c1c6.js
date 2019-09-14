2aaa4cebde7411639eae901bd24e0332d1f9ec40
amralaafcih <amr.alaa@list.ru>
2019-09-14 06:49:08 +0200

    API-Tester Production Built

---
+++ precache-manifest.03c21eeda9bfb5713fb046d339f3c1c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99884d844c55d58ee18f107f8e5e73d8",
+    "url": "/api-tester/index.html"
+  },
+  {
+    "revision": "b33c58df1f91d2395fd4",
+    "url": "/api-tester/static/css/2.463cf3df.chunk.css"
+  },
+  {
+    "revision": "77f2a7ac522340cf79a6",
+    "url": "/api-tester/static/css/main.868d9a1a.chunk.css"
+  },
+  {
+    "revision": "b33c58df1f91d2395fd4",
+    "url": "/api-tester/static/js/2.9e07a752.chunk.js"
+  },
+  {
+    "revision": "77f2a7ac522340cf79a6",
+    "url": "/api-tester/static/js/main.c5ab4f04.chunk.js"
+  },
+  {
+    "revision": "3c75051bd9cb3c4433d7",
+    "url": "/api-tester/static/js/runtime~main.76396295.js"
+  },
+  {
+    "revision": "0f274fa56c97e3f04dc70fb8b8fd9bfc",
+    "url": "/api-tester/static/media/rsuite-icon-font.0f274fa5.woff"
+  }
+]);
